b2bca523c6a352c07f76f48e9f5c2ac470e70008
Orelserre <serre.aurelien26@gmail.com>
2020-07-09 23:19:38 -0400

    many random improvments

--- data_processing.py
+++ data_processing.py
@@ -1,12 +1,30 @@
 """Assembles data from different sources into required format for optim algo.
 
 Takes the raw data from all different sources (gform, Amilia, boat list,
-people skills), and produces all the data required by the optimization
+people list), and produces all the data required by the optimization
 algorithm in the right format.
 """
 
 import pandas as pd
 import numpy as np
+import yaml
+
+
+def people(file):
+    """Load people list.
+
+    file: file containing the list of people, with their weight category, skill
+    class and group
+    """
+    people = pd.read_excel(file)
+    # drop header rows (at begining of comp, master, rec sections)
+    people = people.loc[people["group"].notna()]
+    # drop column that we don't use
+    people.drop(columns="later on", inplace=True)
+    # consider heavy heavy the same as heavy (changes nothing for boat alloc)
+    people.replace({'weight': {'HH': 'H'}}, inplace=True)
+
+    return people
 
 
 def amilia_participants(file):
@@ -85,45 +103,49 @@ def gform_prefs(file, corrections, gform_cols):
     return newprefs
 
 
-def registration_check(gform, amilia):
-    """Cross check people who registered and who answered the gform.
+def intersection(people, amilia, gform, exclude):
+    """Cross check that people appear in all information sources.
 
-    gform: preferences dataframe containing people who answered the form
+    people: dataframe of people for who we have skill and weight info
     amilia: dataframe of people registered on amilia
+    gform: preferences dataframe containing people who answered the form
+    exclude: list of people we won't need to consider
+    Returns:
 
-    Returns :
-        not_reg: (pd.Index) list of people who answered the gform but did not
-            register on amilia
-        no_gform: (pd.DataFrame) list of people who registered but did not
-            answered the gform (with their detailed info)
-        intersection: (pd.DataFrame) people who both answered the gform and
-            registered on amilia
+        check: (pd.DataFrame) one columm per source of data, saying if people
+            appear in this source or not (not including exculded people)
+        people: (pd.DataFrame) people info of the intersection
+        amilia: (pd.DataFrame) amilia info of the intersection
+        gform: (pd.DataFrame) gform answers of the intersection
     """
-    not_reg = gform.loc[gform.index.difference(amilia.index)]
-    no_gform = amilia.loc[amilia.index.difference(gform.index)]
-    intersection = gform.loc[gform.index.intersection(amilia.index)]
+    isin_people = pd.Series(index=people.index, data="ok", name="people")
+    isin_amilia = pd.Series(index=amilia.index, data="ok", name="amilia")
+    isin_gform = pd.Series(index=gform.index, data="ok", name="gform")
+    check = pd.concat(
+        [amilia[["Prénom", "Nom de famille"]], isin_people, isin_amilia, isin_gform], join="outer", axis=1,
+    )
+    # only keep people not in exclude list
+    check = check.loc[~check.index.isin(exclude)]
 
-    return not_reg, no_gform, intersection
+    intersection_index = check.dropna().index
+    people = people.loc[intersection_index]
+    amilia = amilia.loc[intersection_index]
+    gform = gform.loc[intersection_index]
 
+    # replace nan with "missing" storing
+    check = check.fillna("missing")
+    check.sort_index(inplace=True)
 
-def people(file):
-    """Load people list.
-
-    file: file containing the list of people, with their weight class, skill
-    class and group
-    """
-    people = pd.read_excel(file)
-    # drop header rows (at begining of comp, master, rec sections)
-    people = people.loc[people["group"].notna()]
-    # drop column that we don't use
-    people.drop(columns="later on", inplace=True)
-    # consider heavy heavy the same as heavy (changes nothing for boat alloc)
-    people.replace({'weight': {'HH': 'H'}}, inplace=True)
-
-    return people
+    return check, people, amilia, gform
 
 
 def boats(file):
+    """Load boat list.
+
+    file: file containing the list of available skiffs, with their boat class
+    (skills required to row them), and the weight categories they can
+    accommodate
+    """
     boats = pd.read_excel(file, sep=";", header=[1])
     boats.drop(columns=["owner"], inplace=True)
     boats.set_index('name', inplace=True)
@@ -132,3 +154,21 @@ def boats(file):
                   inplace=True)
 
     return boats
+
+
+def boat_skill_map(file):
+    """Load a mapping between boat classes and skills required
+
+    file: yaml file containing a dict with the mapping {skill: boat_class}
+    Returns:
+
+    boat_for_skill: a dict mapping {skill: boat_class}
+    skill_for_boat: a dict mapping {boat_class: skill}
+    """
+    boat_for_skill = yaml.safe_load(file.open("r"))
+    skill_for_boat = dict()
+    for skill, boatclasses in boat_for_skill.items():
+        for bclass in boatclasses:
+            skill_for_boat.setdefault(bclass, []).append(skill)
+
+    return boat_for_skill, skill_for_boat
--- optim.py
+++ optim.py
@@ -1,3 +1,10 @@
+"""Compute additional data required by and run the optimization algorythm.
+
+All functions defined here use the data processed by `data_preprocessing`.
+Some compute additional information from them, and some define the Mixed
+Integer model, and solves it using SCIP and it's PySCIPOpt python interface.
+"""
+
 import pandas as pd
 import numpy as np
 from pyscipopt import Model
@@ -9,20 +16,42 @@ VALUE_SECOND = -1
 
 
 def gen_utility(prefs):
+    """Compute the utility values that will go into the objective function.
+
+    prefs: (pd.DataFrame) first and second choices of people for every time
+    slot.
+    """
     first = prefs['first']
     second = prefs['second']
     # empty dataframe for storing the utilities
     utility = pd.DataFrame(index=prefs.index, columns=first.columns)
-    # set -1 on second choices
+    # set VALUE_SECOND on second choices
     utility.where(~(second == 1), VALUE_SECOND, inplace=True)
-    # set 0 on first choices (override second choices if both are provided
+    # set VALUE_FIRST on first choices (override second choices if both are provided)
     utility.where(~(first == 1), VALUE_FIRST, inplace=True)
 
     return utility
 
 
-def boats_of_people(people, boats, match):
-    boats_of_person = dict()
+def nb_train_asked(utility):
+    """Compute the number of trainings people asked for (in the week).
+
+    The number of trainings people asked for is their number of first choice.
+    """
+    return utility.apply(lambda x: x.value_counts()[VALUE_FIRST], axis=1)
+
+
+def boats_for_people(people, boats, match):
+    """Compute the boats that each person can row.
+
+    people: list of people with their skills and weight category,
+    boats: list of boats with their class and weight category
+    Returns:
+
+    boats_for_person: dict with each person as keys, and the set of boats they
+    can row based on their skills and weight class.
+    """
+    boats_for_person = dict()
     for p in people.index:
         skill = people.loc[p]["skill"]
         weight = people.loc[p]["weight"]
@@ -33,23 +62,23 @@ def boats_of_people(people, boats, match):
         weight_mask = boats[weight] == 1
 
         matching_boats = boats[skill_mask & weight_mask]
-        boats_of_person[p] = set(matching_boats.index)
+        boats_for_person[p] = set(matching_boats.index)
 
-    return boats_of_person
+    return boats_for_person
 
 
-def times_of_people(utility):
-    times_of_person = dict()
+def times_for_people(utility):
+    times_for_person = dict()
     for p in utility.index:
         # available times for the person
-        times_of_person[p] = set(utility.loc[p][utility.loc[p].notna()].index)
+        times_for_person[p] = set(utility.loc[p][utility.loc[p].notna()].index)
 
-    return times_of_person
+    return times_for_person
 
 
 def people_for_boat_time(people, boats, utility, reverse_match):
     # people likely to row a given boat at a given time
-    people_for_boat_time_dict = dict()
+    people_for_boat_time = dict()
     for b in boats.index:
         skill_mask = pd.Series(
             data=False, index=people.index)
@@ -66,26 +95,26 @@ def people_for_boat_time(people, boats, utility, reverse_match):
         people_for_boat = set(people[skill_mask & weight_mask].index)
 
         for t in utility.columns:
-            people_for_boat_time_dict[(b, t)] = {
+            people_for_boat_time[(b, t)] = {
                 p for p in people_for_boat if not np.isnan(utility.loc[p, t])
             }
 
-    return people_for_boat_time_dict
+    return people_for_boat_time
 
 
-def define_model(people, utility, boats, boats_of_people, times_of_people,
-                 people_for_boat_time_dict):
-    # number of trainings people ask for (number of first choice)
-    asked_nb_trainings = utility.apply(lambda x: x.value_counts()[VALUE_FIRST], axis=1)
-
+def define_model(
+        utility, nb_train_asked, boats, boats_for_person, times_for_person,
+        people_for_boat_time
+):
     # definition of the model
     model = Model("boat_alloc")
 
+    people = utility.index
     # defining varialbes
     variables = {}
-    for p in people.index:
-        for b in boats_of_people[p]:
-            for t in times_of_people[p]:
+    for p in people:
+        for b in boats_for_person[p]:
+            for t in times_for_person[p]:
                 variables[(p, b, t)] = model.addVar(vtype="B")
 
     # minimal pref score among all people, that we want to maximize
@@ -93,18 +122,18 @@ def define_model(people, utility, boats, boats_of_people, times_of_people,
 
     # setting the objective fucntion
     total_pref = quicksum(utility.loc[p, t] * variables[(p, b, t)]
-                          for p in people.index
-                          for b in boats_of_people[p]
-                          for t in times_of_people[p])
+                          for p in people
+                          for b in boats_for_person[p]
+                          for t in times_for_person[p])
     model.setObjective(500 * s + total_pref, "maximize")
 
     # adding the constraints
-    for p in people.index:
+    for p in people:
         # sum of prefs
         sum_of_prefs = quicksum(
             utility.loc[p, t] * variables[(p, b, t)]
-            for b in boats_of_people[p]
-            for t in times_of_people[p]
+            for b in boats_for_person[p]
+            for t in times_for_person[p]
         )
         # s = min_{p} sum of prefs
         model.addCons(s <= sum_of_prefs)
@@ -112,38 +141,51 @@ def define_model(people, utility, boats, boats_of_people, times_of_people,
         # each person trains the number of times they asked
         nb_trainings = quicksum(
             variables[(p, b, t)]
-            for b in boats_of_people[p]
-            for t in times_of_people[p]
+            for b in boats_for_person[p]
+            for t in times_for_person[p]
         )
-        model.addCons(nb_trainings <= asked_nb_trainings[p])
+        model.addCons(nb_trainings <= nb_train_asked.loc[p])
 
         # each person can only row one boat at a time
-        for t in times_of_people[p]:
-            nb_boats = quicksum(variables[(p, b, t)] for b in boats_of_people[p])
+        for t in times_for_person[p]:
+            nb_boats = quicksum(
+                variables[(p, b, t)] for b in boats_for_person[p]
+            )
             model.addCons(nb_boats <= 1)
 
         # not am1 and am2 in the same day
         # days where "am2" exists (i.e. not Saturday)
-        weekdays = utility.columns[utility.columns.get_level_values("time") == 'am2'].unique("day")
+        weekdays = utility.columns[
+            utility.columns.get_level_values("time") == 'am2'
+        ].unique("day")
         for day in weekdays:
-            if not np.isnan(utility.loc[p, (day, 'am1')]) and not np.isnan(utility.loc[p, (day, 'am2')]):
-                model.addCons(quicksum(variables[(p, b, (day, 'am1'))] + variables[(p, b, (day, 'am2'))] for b in boats_of_people[p]) <= 1)
+            if not np.isnan(utility.loc[p, (day, 'am1')]) and not np.isnan(
+                    utility.loc[p, (day, 'am2')]
+            ):
+                model.addCons(
+                    quicksum(
+                        variables[(p, b, (day, 'am1'))]
+                        + variables[(p, b, (day, 'am2'))]
+                        for b in boats_for_person[p]
+                    ) <= 1
+                )
 
     # for each boat and time, no more than one person
     # for b in set(boats[boats["boat_class"].isin([0, 1, 2])].index):
     for b in boats.index:
         for t in utility.columns:
             nb_people_on_boat_time = quicksum(
-                variables[(p, b, t)] for p in people_for_boat_time_dict[(b, t)]
+                variables[(p, b, t)] for p in people_for_boat_time[(b, t)]
             )
             model.addCons(nb_people_on_boat_time <= 1)
 
     return model, variables, s
 
 
-def optimize(model, variables, s, people, utility, boats_of_people, times_of_people):
-    # number of trainings people ask for (number of first choice)
-    asked_nb_trainings = utility.apply(lambda x: x.value_counts()[VALUE_FIRST], axis=1)
+def optimize(
+    model, variables, s, utility, nb_train_asked, boats_for_person,
+    times_for_person
+):
 
     model.optimize()
 
@@ -153,18 +195,29 @@ def optimize(model, variables, s, people, utility, boats_of_people, times_of_peo
         result = pd.DataFrame(columns=utility.columns, index=utility.index)
         result.index.rename("people", inplace=True)
 
-        fairness = pd.DataFrame(0, index=utility.index, columns=['nb_asked', 'nb_first', 'nb_second'])
-        fairness['nb_asked'] = asked_nb_trainings
+        fairness = pd.DataFrame(
+            0, index=utility.index,
+            columns=['nb_asked', 'nb_first', 'nb_second']
+        )
+        fairness['nb_asked'] = nb_train_asked
 
-        for p in people.index:
-            for t in times_of_people[p]:
-                boat = {b for b in boats_of_people[p] if
-                        model.isEQ(model.getVal(variables[(p, b, t)]), 1)}
+        for p in utility.index:
+            for t in times_for_person[p]:
+                boat = {
+                    b
+                    for b in boats_for_person[p]
+                    if model.isEQ(model.getVal(variables[(p, b, t)]), 1)
+                }
                 assert len(boat) <= 1
                 if boat:
                     result.loc[p, t] = boat.pop()
                 # if p is rowing at time t
-                if model.isEQ(model.getVal(quicksum(variables[(p, b, t)] for b in boats_of_people[p])), 1):
+                if model.isEQ(
+                    model.getVal(
+                        quicksum(variables[(p, b, t)] for b in boats_for_person[p])
+                    ),
+                    1
+                ):
                     if utility.loc[p, t] == VALUE_FIRST:
                         fairness.loc[p, 'nb_first'] += 1
                     elif utility.loc[p, t] == VALUE_SECOND:
@@ -172,12 +225,16 @@ def optimize(model, variables, s, people, utility, boats_of_people, times_of_peo
                     else:
                         raise
 
-        fairness["diff"] = fairness['nb_first'] + fairness['nb_second'] - fairness['nb_asked']
+        fairness["diff"] = (
+            fairness['nb_first'] + fairness['nb_second'] - fairness['nb_asked']
+        )
 
         # result.to_excel("results/comp/results_boat.xlsx")
         print("fairness score (0 = optimal) : ", model.getVal(s))
 
+        return result, fairness
+
     else:
         print(f"solver finished with status {status}")
 
-    return result, fairness
+        return None, None
